713740042ac7cc54e15a0ead9793f344741db8c1
Alsia Mary George <alsiamarygeorge@gmail.com>
2026-01-17 22:54:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function calculate() {
+  const total = Number(document.getElementById("total").value);
+  const attended = Number(document.getElementById("attended").value);
+  const target = Number(document.getElementById("target").value);
+
+  if (total <= 0 || attended < 0 || attended > total) {
+    document.getElementById("result").innerText =
+      "Invalid input. Fix your numbers.";
+    return;
+  }
+
+  const current = ((attended / total) * 100).toFixed(2);
+  const p = target / 100;
+  let x = Math.ceil(((p * total) - attended) / (1 - p));
+  if (x < 0) x = 0;
+
+  document.getElementById("result").innerHTML = `
+    <p>Current Attendance: <b>${current}%</b></p>
+    <p>You must attend the next <b>${x}</b> classes continuously.</p>
+  `;
+}
